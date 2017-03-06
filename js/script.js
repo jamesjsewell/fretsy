2ce450f1cc7bb2afd83fcec33ef93566b156024a
james sewell <jamesjosephsewell@gmail.com>
2017-03-06 10:07:23 -0600

    getting information from the server that is filtered into guitar related items, attempting to fetch a single listing from the server

--- js/script.js
+++ js/script.js
@@ -2,20 +2,27 @@ var apiKey = 'qycca7qp2gj078smbdvxdg7b'
 var contentNode = document.querySelector('.content')
 var headerNode = document.querySelector('.header')
 
-//---------------HTML TOOLING--------------------//
+//---------------HTML TOOLING-------------------//
 
-HtmlTemplate = function(t_g, clss, i_d, con, hrf){
-    
+HtmlTemplate = function(t_g, clss, i_d, hrf, type, con){
+
     this.tag = '<' + t_g + " ",
     this.class = 'class = ' + '"' + clss + '" ',
     this.id = 'id = ' + '"' + i_d + '" ',
-    this.href = 'href = ' + hrf
+    this.href = 'href = ' + hrf 
+    this.type = 'type = ' + '"' + type + '"'
     this.content = con,
     this.close = '<' + '/' + t_g + '>'
+
 }
 
-//----------------------------------------------//
-//-------------------VIEWS---------------------//
+function templateToString(htmlModel){
+    return htmlModel.tag + htmlModel.class + htmlModel.id + htmlModel.href + htmlModel.type + ">" + htmlModel.content + htmlModel.close
+}
+
+//------------------------------------------------//
+
+//-------------------VIEWS------------------------//
 
 //home page 
 var HomepageView = Backbone.View.extend({
@@ -25,40 +32,22 @@ var HomepageView = Backbone.View.extend({
     },
     render: function(){
         
-        var arrayOfModels = this.collection.models[0]
-
-        var htmlString = ""
-
-        for(var i = 0; i < arrayOfModels; i++){
-
-            var currentModel = arrayOfModels[i]
-            var descriptionHtml = new HtmlTemplate('div','listing', 'listing'+i, currentModel.description)
-            var titleHtml = new HtmlTemplate('h2','listingTitle', 'listingTitle'+i, currentModel.description)
-        
-        }
-
-        console.log(descriptionHtml)
-
-
-
-
-
-
-
-
-
-
+        var arrayOfModels = this.collection.models
+        build_listings_html(arrayOfModels)
 
     },
 })
 
 //search results
 var SearchResultsView = Backbone.View.extend({
+
     initialize: function(){
         this.listenTo(this.collection, 'sync', this.render)
     },
     render: function(){
-        console.log('rendered search results')
+
+        var arrayOfModels = this.collection.models
+        build_listings_html(arrayOfModels)      
     }
     
 })
@@ -66,22 +55,75 @@ var SearchResultsView = Backbone.View.extend({
 //item view
 var ItemView = Backbone.View.extend({
     initialize: function(){
-        this.listenTo(this.collection, 'sync', this.render)
+        this.listenTo(this.model, 'sync', this.render)
     },
     render: function(){
-        console.log('rendered search results')
+        
+        build_item_html(this.model)
     },
 })
+//------------------------------------------------//
+
+//-----------------/BUILD HTML/-------------------//
+
+function build_listings_html(arrayOfModels){
+
+    htmlString = ""
+
+    var logoHtml = new HtmlTemplate('h2', 'logoText', 'logoText1', 'none', 'none', 'FRETSY'),
+    searchHtml = new HtmlTemplate('input', 'searchField', 'seachField1', '', 'search', 'none')
+    var logoString = templateToString(logoHtml),
+    searchString = templateToString(searchHtml)
+
+    var headerHtml = new HtmlTemplate('div', 'header', 'homeHeader', 'none', 'none', logoString + searchString)
+    var headerString = templateToString(headerHtml)
+    headerNode.innerHTML = headerString
+
+    for(var i = 0; i < arrayOfModels.length; i++){
+    
+        //assemble html
+        var currentModel = arrayOfModels[i],
+        modelProperties = currentModel.attributes,
+        descriptionHtml = new HtmlTemplate('p','listingDescription', 'listingDescription'+i, 'none', 'none', modelProperties.description),   
+        titleHtml = new HtmlTemplate('h3','listingTitle', modelProperties.listing_id, 'none', 'none', modelProperties.title)      
+
+        //convert assembled html to strings
+        var titleString = templateToString(titleHtml),
+        descriptionString = templateToString(descriptionHtml)
+        
+        //add all html strings into a container
+        var allHtml = titleString + descriptionString,
+        postHtml = new HtmlTemplate('div', 'listingPost', 'listingPost'+i, 'none', 'none', allHtml)
+        postString = templateToString(postHtml)
+
+        htmlString += postString
+    }
+
+    //applyHtml
+    var pageHtml = new HtmlTemplate('div', 'homePageContent', 'contentContainer', 'none', 'none', htmlString),
+    pageString = templateToString(pageHtml)
+
+    contentNode.innerHTML = pageString
+
+    user_search()
+    user_select()
+
+}
+
+function build_item_html(itemModel){
+    console.log(itemModel)
+    console.log(itemModel.title, "titlelll")
+}
 
 //---------------------------------------------------------//
 
-//-----------------MODELS AND COLLECTIONS--------------------//
+//----------------/MODELS AND COLLECTIONS/-----------------//
 
 var HomepageCollection = Backbone.Collection.extend({
 
     url: 'https://openapi.etsy.com/v2/listings/active.js',
     parse: function(response) {
-        console.log(response)
+        
         return response.results
     }
 });
@@ -97,12 +139,13 @@ var SearchCollection = Backbone.Collection.extend({
 
 var ItemModel = Backbone.Model.extend({
 
-    url: 'https://openapi.etsy.com/v2/listings/active.js',
+    url: 'https://openapi.etsy.com/v2/listings/',
     parse: function(response) {
-        console.log(response)
-        return response.results
+
+        return response.results.attributes
     }
 });
+
 //------------------------------------------------------//
 
 //--------------------/CONTROLLER/----------------------//
@@ -112,13 +155,14 @@ var AppRouter = Backbone.Router.extend({
     routes: {
 
         "home": "homePage",
-        "search/:query": "searchResults"
+        "search/:query": "searchResults",
+        "item/:query": "detailsOfItem"
         // matches http://example.com/#anything-here
     },
 
     homePage: function(){
 
-        homeCollectionInstance = new SearchCollection
+        homeCollectionInstance = new HomepageCollection
         homeCollectionInstance.fetch({ 
 
             dataType: 'jsonp',
@@ -128,7 +172,7 @@ var AppRouter = Backbone.Router.extend({
                 'tags': 'guitar'    
             }
         })
-        console.log(homeCollectionInstance.url)
+       
         //puts a collection on the home page
         new HomepageView({
 
@@ -148,7 +192,7 @@ var AppRouter = Backbone.Router.extend({
 
             data: {
                 'api_key': apiKey,
-                'q=': selection
+                'tags': 'guitar' + selection
             }
         })
 
@@ -170,7 +214,7 @@ var AppRouter = Backbone.Router.extend({
 
             data: {
                 'api_key': apiKey,
-                'q': 'guitar'
+                'listing_id': selection, 
             }
         })
 
@@ -182,6 +226,40 @@ var AppRouter = Backbone.Router.extend({
 
         }
 });
+
+//---------------------------------------------------------//
+
+//----------------------/USER INPUT/-----------------------//
+
+function user_search(){
+
+    var searchNode = document.querySelector('.searchField')
+    searchNode.addEventListener('keydown', function(eventObj){
+        
+        if(eventObj.keyCode === 13){
+            var userSearch = ""
+            userSearch = eventObj.target.value  
+            location.hash = "search/" + userSearch
+            eventObj.target.value = ""
+        }
+
+        })
+}
+
+function user_select(){
+
+    window.addEventListener('click', function(clickObj){
+        
+        if(clickObj.target.attributes.class.value === "listingTitle"){
+            var userSelect = clickObj.target.attributes.id.value
+            location.hash = "item/" + userSelect
+        
+        }
+
+        })
+}
+
+//-----------------------------------------------------------//
 
 // Initiate the router
 var app_router = new AppRouter;
